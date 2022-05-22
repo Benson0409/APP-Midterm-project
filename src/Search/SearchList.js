07899f26bffda5c60854286c9bed113210853a0a
Benson0409 <83742453+Benson0409@users.noreply.github.com>
2022-05-22 20:22:32 +0800

    favorite 頁面初步寫入

--- src/Search/SearchList.js
+++ src/Search/SearchList.js
@@ -12,24 +12,6 @@ const SearchList = ({ list, navigation }) => {
       borderBottomRadius={20}
       _dark={{ bg: "#3F382E" }}
       _light={{ bg: "#A1917A" }}
-      ListHeaderComponent={
-        <>
-          <HStack mt={10} justifyContent="center">
-            <Box mr={2} mt={0.5}>
-              <MaterialIcons name="search" color={"#FCAA87"} size={32} />
-            </Box>
-
-            <Input
-              placeholder="搜尋國北附近美食"
-              w={280}
-              borderRadius={10}
-              borderWidth={2}
-              fontSize={"13"}
-              // bg={"white"}
-            ></Input>
-          </HStack>
-        </>
-      }
       data={list}
       renderItem={renderItem}
       keyExtractor={(item) => item.photo}
